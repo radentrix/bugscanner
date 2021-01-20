2f3c00a0ebb4390a050d4ad5a2dcb321e3334895
radentrix <76752628+radentrix@users.noreply.github.com>
2021-01-20 11:55:59 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="bugscanner",
+	version="0.1.5",
+	author="aztecrabbit",
+	author_email="ars.xda@gmail.com",
+	description="Bug Scanner for Internet Freedom (Domain Fronting, Server Name Indication, Etc)",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/aztecrabbit/bugscanner",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+	entry_points={
+		'console_scripts': [
+			'bugscanner=bugscanner.bugscanner:main',
+		],
+	},
+)
+
